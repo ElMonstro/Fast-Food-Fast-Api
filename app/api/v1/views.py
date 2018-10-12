a5fef1916ad3049971054973a1dd36aa7159bff3
Lewis Okeo <jratcher@gmail.com>
2018-10-12 18:30:32 +0300

    Add post function

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,19 +1,26 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request, make_response
 from flask_api import FlaskAPI
 from flask_restful import Api, Resource
+from uuid import uuid1
 
-v1 = Blueprint('v1', __name__, url_prefix='/api/v1/')
+v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(v1)
+
+orders = {}
 
 class Orders(Resource):
     def get(self):
         return {'message':'get request'}
 
     def post(self):
-        pass
+        data = request.get_json()
+        order_no = int(uuid1())
+        orders[order_no] = [data['name'],data['items']]
+        return orders, 201
         
-
-api.add_resource(Orders, '/orders')
+        
+        
+api.add_resource(Orders, '/orders', )
 
 
 
