da4e3937bc93fbeb2e1077376c303e08eeb87c0a
Lewis Okeo <jratcher@gmail.com>
2018-10-16 15:41:38 +0300

    Add setup file ansd fix travis.yml

--- Tests/api/v1/test_views.py
+++ Tests/api/v1/test_views.py
@@ -5,6 +5,11 @@ from app.api import app
 class OrdersTestCase(unittest.TestCase):
     """This class represents the orders testcase"""
     def setUp(self):
-        """Define test variables and initialize app."""
-        pass
+        """Define test variables"""
+        self.app = app
+        self.client = self.app.test_client
+        self.order = {"name":"Jay","items":[["coke", 4],["pizza",5]]}
+        
+
+    def 
         
