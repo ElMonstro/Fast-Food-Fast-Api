0b005239317b9fe55b15cb7e55be1e89536f918f
Lewis Okeo <jratcher@gmail.com>
2018-10-16 15:51:19 +0300

    Add test functions skeleton

--- Tests/api/v1/test_views.py
+++ Tests/api/v1/test_views.py
@@ -9,7 +9,22 @@ class OrdersTestCase(unittest.TestCase):
         self.app = app
         self.client = self.app.test_client
         self.order = {"name":"Jay","items":[["coke", 4],["pizza",5]]}
-        
 
-    def 
+
+    def test_create_order(self):
+        """Test Post request on the api"""
+        pass
+
+    def test_return_list_of_orders(self):
+        """Test get all orders request"""
+        pass
+
+    def test_get_specific_order(self):
+        """Tests if the API can get an order from its id(GET request)"""
+        pass
+
+    def test_edit_order_status(self):
+        """Tests if the api can edit the orderstatus(PUT request)"""
+        pass
+
         
