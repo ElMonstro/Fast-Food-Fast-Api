559705644df2bc329ddff1b670911abe2736d46d
Lewis Okeo <jratcher@gmail.com>
2018-10-16 14:46:40 +0300

    Add Test class

--- Tests/api/v1/test_views.py
+++ Tests/api/v1/test_views.py
@@ -1,1 +1,10 @@
 from app.api.v1.views import Order, Orders
+import unittest
+from app.api import app
+
+class OrdersTestCase(unittest.TestCase):
+    """This class represents the orders testcase"""
+    def setUp(self):
+        """Define test variables and initialize app."""
+        pass
+        
