37c0589e23ea761ba287fe079d9d294be975e97f
Lewis Okeo <jratcher@gmail.com>
2018-10-19 15:51:56 +0300

    Add test for put

--- test_views.py
+++ test_views.py
@@ -28,11 +28,17 @@ class OrdersTestCase(unittest.TestCase):
 
     def test_get_specific_order(self):
         """Tests if the API can get an order from its id(GET request)"""
+        response = self.client.get('/api/v1/orders/b194c012-d399-11e8-a4f6-24fd52059abd')
+        data = json.loads(response.data)
+        orders = {'order':{'b194c012-d399-11e8-a4f6-24fd52059abd': ['Jay', [['coke', 4], ['pizza', 5]], False]}}
+        self.assertEqual(data, orders)
+        self.assertEqual(response.status_code, 200)
         
 
     def test_edit_order_status(self):
         """Tests if the api can edit the orderstatus(PUT request)"""
-        pass
+        response = self.client.put('/api/v1/orders/b194c012-d399-11e8-a4f6-24fd52059abd')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
