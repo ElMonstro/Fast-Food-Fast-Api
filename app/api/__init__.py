6c2cf1abbeda85d8861a98721513d61eb8c5c183
Lewis Okeo <jratcher@gmail.com>
2018-10-21 16:49:40 +0300

    Edit to accomodate config

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from app.api.v1.views import v1, api
-import os
-import sys
+from instance.config import Configuration
 
-file_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(file_dir)
 
-application = Flask(__name__)
+application = Flask(__name__, instance_relative_config=True)
+application.config.from_object(Configuration)
+application.config.from_pyfile('config.py')
 application.register_blueprint(v1)
