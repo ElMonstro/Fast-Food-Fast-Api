8ec89f055781f25d487d3fde05c4110e85b5abb0
Lewis Okeo <jratcher@gmail.com>
2018-10-23 10:14:04 +0300

    Create db functions

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -13,3 +13,33 @@ class DbaseConnection:
             self.conn = psycopg2.connect(conn_string)
         except:
             print('Connection not successful')
+        
+        self.cursor = self.conn.cursor()
+        try:
+            sql_string = 'CREATE TABLE orders test(id PRIMARY KEY, customer_name varchar, items JSON,)'
+            self.cursor.execute(sql_string)
+        except:
+            print('Table not created')
+        
+
+    def save(self):
+        self.conn.commit()
+    
+    def insert(self, table, item):
+        pass
+
+
+    
+    def get_all_items(self, table):
+        sql_string = 'SELECT * FROM {}'.format(table)
+        self.cursor.execute(sql_string)
+
+    def get_item(self, id, table):
+        sql_string = 'SELECT * FROM {} WHERE ID = {}'.format(table, str(id))
+        self.cursor.execute(sql_string)
+
+    
+
+        
+        
+
---
+++ test_models.py
@@ -0,0 +1,2 @@
+from app.api.v1.models import DbaseConnection
+dbase = DbaseConnection()
