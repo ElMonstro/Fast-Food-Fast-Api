ea95081f47e7e6c0974963af7b3ef8d8d6fdc8ad
Lewis Okeo <jratcher@gmail.com>
2018-10-25 18:23:14 +0300

    Add code to function insert order

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,4 +1,5 @@
 import psycopg2
+import json
 from app.api import application
 
 class DbaseConnection:
@@ -9,14 +10,18 @@ class DbaseConnection:
         user = application.config['USER']
         password = application.config['PASSWORD']
         try:
-            conn_string = 'dbname={} user={} host={} password={}'.format(db_name, user, host, password)
+            conn_string = 'dbname={} user={} host={} password={};'.format(db_name, user, host, password)
             self.conn = psycopg2.connect(conn_string)
         except:
             print('Connection not successful')
         
         self.cursor = self.conn.cursor()
         try:
-            sql_string = 'CREATE TABLE orders test(id PRIMARY KEY, customer_name varchar, items JSON,)'
+            sql_string = '''
+            DROP TABLE IF EXISTS orders;
+            DROP TABLE IF EXISTS users;
+            CREATE TABLE orders(id int auto_increment PRIMARY KEY, email varchar(255), items JSON,);
+            CREATE TABLE users(id int PRIMARY KEY, email varchar(255), password varchar(255),);'''
             self.cursor.execute(sql_string)
         except:
             print('Table not created')
@@ -25,18 +30,27 @@ class DbaseConnection:
     def save(self):
         self.conn.commit()
     
-    def insert(self, table, item):
-        pass
+    def insert_order(self, item):
+        item_json = json.dumps(item[1])
+        sql_string = 'INSERT INTO orders(email, items) VALUES({}, {})'.format(item[0], item_json)
+        try:
+            self.cursor.execute(sql_string)
+        except:
+            print('Item not inserted')
+        
 
     
     def get_all_items(self, table):
         sql_string = 'SELECT * FROM {}'.format(table)
         self.cursor.execute(sql_string)
+        items = self.cursor.fetchall()
+        return items
 
     def get_item(self, id, table):
         sql_string = 'SELECT * FROM {} WHERE ID = {}'.format(table.upper, str(id))
         self.cursor.execute(sql_string)
-
+        item = self.cursor.fetchone()
+        return item
     
 
         
